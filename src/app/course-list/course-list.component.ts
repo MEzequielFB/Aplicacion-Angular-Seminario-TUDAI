ac01f112eb9fb595eb17ecb6c4ab465ebd09d67c
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-09-22 01:23:34 -0300

    Agrega arreglo de objetos, uso de directivas ngFor y ngClass

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './Course';
 
 @Component({
   selector: 'app-course-list',
@@ -8,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
 export class CourseListComponent implements OnInit {
 
   title: String;
+  courses: Course[];
+  /* courses:Array<{title: String, price: number, description: String, image: String}>; */
 
   constructor() {
 
-    this.title = "Título de la lista";
+    this.title = "Cursos recomendados";
+    this.courses = [
+      {
+        title: "Curso Python",
+        price: 1500,
+        description: "Curso para aprender Python asd dasfas fasfas",
+        image: {
+          path: "assets/img/Python-P.webp",
+          alt: "Logo Python"
+        },
+        clearance: true
+      },
+      {
+        title: "Curso Java",
+        price: 1450,
+        description: "Curso para aprender Java",
+        image: {
+          path: "assets/img/java.jpg",
+          alt: "Logo Java"
+        },
+        clearance: false
+      },
+      {
+        title: "Curso PHP",
+        price: 1450,
+        description: "Curso para aprender PHP",
+        image: {
+          path: "assets/img/php.jpg",
+          alt: "Logo PHP"
+        },
+        clearance: false
+      },
+    ]
   }
 
   ngOnInit(): void {
